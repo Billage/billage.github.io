cd523ecba5fbf23a13714d18a20c1374e9c2f827
Green_tEA <northwood89@gmail.com>
2016-12-28 17:24:43 +0900

    Add files via upload

--- js/effect.js
+++ js/effect.js
@@ -3,28 +3,34 @@ $(".gnb li ul,.link,.m_gnb li ul").hide();
 $(".gnb>li").hover(function(){
         $('ul',this).stop().slideToggle(100);
         });
-});
-$(function(){
+
 $(".m_gnb>li").click(function(){
         $('ul',this).stop().slideToggle(100);
         });
-});
-$(function(){
-$("article ul li").hover(function(){
+
+$(".section1 article ul li").hover(function(){
 $(".zoom",this).stop().css("transform","scale(1.2)").fadeTo(200,0.6)},function(){
 $(".zoom",this).stop().css("transform","scale(1)").fadeTo(200,0.8)});
-});
-$(function(){
-$("article ul li").hover(function(){
+
+$(".section1 article ul li,.bread article").hover(function(){
 $(".link",this).stop().fadeTo(200,1)},function(){
 $(".link",this).stop().fadeTo(200,0)});
-});
-$(function(){
+
     var hamburger = $('.toggle');
     hamburger.stop().click(function(event) {
         $(this).toggleClass('on');
         $(".m_gnb_wrap").toggleClass('on');
     });
+
+    $(window).resize(function (){
+	var width = window.outerWidth;
+	if (width > 1024) {
+		$(".m_gnb_wrap,.toggle").removeClass('on');
+	}
+	});
+
+    
+
 });
 // var ww = $(window).width();
 // $(function(){
